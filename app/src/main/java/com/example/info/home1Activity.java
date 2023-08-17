390c999d26ba23ddb676a551e7505267992272d8
Park Sihyun <32purp1nk@gmail.com>
2023-08-17 17:48:40 +0900

    Initial commit

---
+++ app/src/main/java/com/example/info/home1Activity.java
@@ -0,0 +1,13 @@
+package com.example.info;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home1);
+    }
+}
---
+++ app/src/main/java/com/example/info/oneActivity.java
@@ -0,0 +1,14 @@
+package com.example.info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class oneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
